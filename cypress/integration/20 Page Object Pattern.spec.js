fa02e324cc193a043cc18ca23be404b6209f2573
Kamil Grabiec <kamil.grabiec@pl.abb.com>
2022-06-13 14:18:02 +0200

    first object pattern

--- cypress/integration/20 Page Object Pattern.spec.js
+++ cypress/integration/20 Page Object Pattern.spec.js
@@ -1,1 +1,21 @@
 /// <reference types="cypress" />
+
+import { navigateTo } from "../support/page_objects/navigationPage"
+
+
+
+describe("", () => {
+    beforeEach('Open application', () => {
+        cy.visit("/")
+    })
+
+    it("verify navigations across the pages", () =>{
+        navigateTo.formLayoutsPage();
+        navigateTo.datepickersPage();
+        navigateTo.smartTablePage();
+        navigateTo.toastrPage();
+        navigateTo.tooltipPage();
+        
+    })
+
+})
